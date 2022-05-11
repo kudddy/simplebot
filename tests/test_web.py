4e6ef07c7544d072451156124404c29310df188f
16783869 <Khudyakov.K.Ko@sberbank.ru>
2022-05-11 13:10:50 +0300

    app tests for web-app

--- tests/test_web.py
+++ tests/test_web.py
@@ -1,14 +1,37 @@
-from aiohttp.test_utils import AioHTTPTestCase
-from aiohttp import web
+from aiohttp.test_utils import AioHTTPTestCase, unittest_run_loop
 import logging
 
 from plugins.core.start import Updater
-
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
 log.setLevel(logging.DEBUG)
+
+
+def generate_payload(text: str):
+    return {
+        "update_id": 243475549,
+        "message": {
+            "message_id": 9450,
+            "from": {
+                "id": 81432612,
+                "is_bot": False,
+                "first_name": "Kirill",
+                "username": "kkkkk_kkk_kkkkk",
+                "language_code": "ru"
+            },
+            "chat": {
+                "id": 81432612,
+                "first_name": "Kirill",
+                "username": "kkkkk_kkk_kkkkk",
+                "type": "private"
+            },
+            "date": 1589404439,
+            "text": text
+        }
+    }
+
 
 class MyAppTestCase(AioHTTPTestCase):
 
@@ -16,40 +39,20 @@ class MyAppTestCase(AioHTTPTestCase):
         """
         Override the get_app method to return your application.
         """
-
-        # async def hello(request):
-        #     return web.Response(text='Hello, world')
-        #
-        # app = web.Application()
-        # app.router.add_get('/', hello)
         return Updater._create_app_hook()
 
-    async def test_example(self):
-        print("fsdfsdf")
-        data = {
-            "update_id": 243475549,
-            "message": {
-                "message_id": 9450,
-                "from": {
-                    "id": 81432612,
-                    "is_bot": False,
-                    "first_name": "Kirill",
-                    "username": "kkkkk_kkk_kkkkk",
-                    "language_code": "ru"
-                },
-                "chat": {
-                    "id": 81432612,
-                    "first_name": "Kirill",
-                    "username": "kkkkk_kkk_kkkkk",
-                    "type": "private"
-                },
-                "date": 1589404439,
-                "text": "Подарок"
-            }
-        }
-        log.debug("dsdsfdsfs")
-        async with self.client.request("POST", "/tlg/", json=data) as resp:
+    @unittest_run_loop
+    async def test_step_one(self):
+        async with self.client.request("POST", "/tlg/", json=generate_payload("подарок")) as resp:
             self.assertEqual(resp.status, 200)
             text = await resp.text()
             print(text)
-        self.assertIn("Hello, world", text)
+        self.assertIn("ok", text)
+
+    @unittest_run_loop
+    async def test_step_two(self):
+        async with self.client.request("POST", "/tlg/", json=generate_payload("подарок")) as resp:
+            self.assertEqual(resp.status, 200)
+            text = await resp.text()
+            print(text)
+        self.assertIn("ok", text)
